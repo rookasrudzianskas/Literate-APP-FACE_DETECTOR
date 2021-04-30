3c7e1ac9664bfb47dbd4c8f47b47f02ee84241b5
Rokas <rookas.rudzenskas@gmail.com>
2021-04-30 18:24:46 +0300

    Rokas

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# choosing an image, and calling the opecv imread function
+img = cv2.imread('rokas.png')
 
-
+cv2.imshow('ROkas!', img)
+cv2.waitKey()
 
 
 
