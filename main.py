0cc9f5103318e2d95b240492a2d2213e682e62aa
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 21:23:48 +0300

    Rokas

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ while True:
     grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
     for (x, y, w, h) in face_coordinates:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(128, 256), randrange(256), randrange(256)), 2)
 
