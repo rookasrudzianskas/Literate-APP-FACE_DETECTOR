ef21b8cb04c00ba33ebcacb07b7e599791dacd2a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 14:09:18 +0300

    Roks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+from random import randrange
 
 trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 # choosing an image, and calling the opecv imread function
@@ -14,8 +15,12 @@ face_coordinates = trained_face_data.detectMultiScale(grayscailed_image)
 
 # draw rectangles
 # two coordinates, color, and the thickness of rectangle
-(x, y, w, h) = face_coordinates[2]
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# two coordinates and width and height
+# (x, y, w, h) = face_coordinates[2]
+
+# looping per all faces
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
 
 print(face_coordinates)
 
